6c71a6467f3ded9d793fc409ee850758b525ac1d
JavoxirRabbonayev <rabbonayevjavoxir1078@gmail.com>
2024-01-28 12:09:45 +0100

    first commit

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+     
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque sed itaque ea laboriosam minus commo
+        di soluta, molestias illo non iusto dolores voluptas sunt quae facilis quas praesentium. Sequi, cum odio
+        </p>
+    
+</div>
---
+++ resources/views/components/form/page-header.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+    <div class="container-fluid page-header-inner py-5">
+        <div class="container text-center pb-5">
+            {{$slot}}
+        </div>
+    </div>
+</div>
